46560bc30f526ea20fc9a858339112ca849a6254
florentin-ap <florentin.purnama@traveloka.com>
2021-12-10 14:48:11 +0700

    Add flipr schema.

---
+++ verify/appschema.sql
@@ -0,0 +1,21 @@
+-- Verify flipr:appschema on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+SELECT pg_catalog.has_schema_privilege('flipr', 'usage');
+-- SELECT pg_catalog.has_schema_privilege('nonesuch', 'usage');
+-- SELECT 1/COUNT(*) FROM information_schema.schemata WHERE schema_name = 'flipr';
+-- SELECT 1/COUNT(*) FROM information_schema.schemata WHERE schema_name = 'nonesuch';
+
+ROLLBACK;
+
+
+
+-- DO
+-- DECLARE
+--     result varchar;
+-- BEGIN
+--    result := (SELECT name FROM flipr.pipelines WHERE id = 1);
+--    ASSERT result = 'Example';
+-- END;
